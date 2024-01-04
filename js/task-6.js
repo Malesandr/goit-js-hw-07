a4878dc9d1912d737085b98b7dfb95d738e86ee2
Malesandr <aleksandr.mikolenko@gmail.com>
2024-01-05 00:35:26 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -6,5 +6,33 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = '';
 
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${30 + i * 10}px`;
+    div.style.height = `${30 + i * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(div);
+  }
+}
 
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+const buttonCreate = document.querySelector("button[data-create]")
+  buttonCreate.addEventListener("click", () => {
+  const input = document.querySelector("input");
+  const amount = parseInt(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } 
+});
+
+document.querySelector("button[data-destroy]").addEventListener("click", destroyBoxes); 
